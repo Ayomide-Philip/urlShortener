ddf3f0073b6fccfa975a40bc2d0db699adc3891c
Areo Ayomide <areoayomide2008@gmail.com>
2025-05-26 16:36:13 +0100

    refactor: update App component layout and functionality for URL shortening

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,51 @@
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
-      <div className="containermx-auto max-w-xl p-6">
+      <div className="container mx-auto max-w-xl p-6">
         <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-          <h1 className="text-2xl font-semibold mb-4 text-gray-100">Apply Coupon Code</h1>
+          <h1 className="text-2xl font-semibold mb-4 text-gray-100">
+            Url Shortener
+          </h1>
 
           <div className="mb-4">
-            <label htmlFor="coupon" className="block text-gray-300 font-semibold mb-2">
-              Coupon Code:
+            <label
+              htmlFor="url"
+              className="block text-gray-300 font-semibold mb-2"
+            >
+              Your Url:
             </label>
             <input
               type="text"
-              id="coupon"
-              name="coupon"
+              id="url"
+              name="url"
               className="w-full px-4 py-2 border border-gray-700 bg-gray-900 text-gray-100 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Enter your coupon code"
+              placeholder="Enter your long url"
             />
           </div>
 
           <div className="text-center">
             <button className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500">
-              Apply Coupon
+              Generate Short Url
             </button>
           </div>
 
-          <div className="mt-4 text-green-400">
-            Coupon code applied successfully! You saved $10.
+          <div className="mb-4 mt-5">
+            <div className="relative flex">
+              <input
+                type="text"
+                id="short-url"
+                name="short-url"
+                className="w-full px-4 py-2 border border-gray-700 bg-gray-900 text-gray-100 rounded-l-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Generated Short Url"
+                readOnly
+              />
+              <button
+                className="bg-blue-600 text-white px-4 py-2 rounded-r-lg hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
+                style={{ marginLeft: "-1px" }}
+              >
+                Copy
+              </button>
+            </div>
           </div>
         </div>
       </div>
